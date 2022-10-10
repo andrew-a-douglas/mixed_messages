11a531e771a1aec653828fccb4030cdcedf63336
Andrew Douglas <andrew.a.douglas@gmail.com>
2022-10-10 17:42:22 -0400

    Add stoic quotes to object

---
+++ mixed-msg.js
@@ -0,0 +1,24 @@
+const quotes = [
+        {
+            msg: 'Waste no more time arguing what a good man should be. Be One.',
+            author: 'Marcus Aurelius'
+        }, 
+        {
+            msg: 'We are more often frightened than hurt; and we suffer more in imagination than in reality.',
+            author: 'Seneca'
+        }, 
+        {
+            msg: 'Do not seek for everything to happen as you wish it would, but rather wish that everything happens as it actually will—then your life will flow well.',
+            author: 'Epictetus'
+        }, 
+        {
+            msg: 'I begin to speak only when I am certain what I will say is not better left unsaid.',
+            author: 'Cato'
+        }, 
+        {
+            msg: 'What man actually needs is not a tensionless state but rather the striving and struggling for some goal worthy of him.',
+            author: 'Victor Frankl'
+        }
+    ]
+
+console.log(quotes[4].msg +  ' -- ' + quotes[4].author);
